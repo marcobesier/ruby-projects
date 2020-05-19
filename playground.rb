d52766a698ba38abcebd65e546fe75204a354f5e
Marco Besier <marcobesier@icloud.com>
2020-05-19 06:50:23 +0200

    Add comments to print statements

---
+++ playground.rb
@@ -0,0 +1,14 @@
+# prints without a line break, returns nil
+# print "Hello World!"
+
+# prints with a line break, returns nil
+# puts "Hello World!"
+
+# prints with a line break, returns the given string
+# p "Hello World!"
+
+# def say_hello(greeting)
+#   puts greeting
+# end
+
+# say_hello "Hello Marco!"
