3cc6691521b95bd1765bf3e30417e67a520f69ce
Marco Besier <marcobesier@icloud.com>
2020-05-26 10:06:57 +0200

    Add functions for math operations

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -1,11 +1,31 @@
+def add(first_num, second_num)
+  first_num.to_f + second_num.to_f
+end
+
+def subtract(first_num, second_num)
+  first_num.to_f - second_num.to_f
+end
+
+def multiply(first_num, second_num)
+  first_num.to_f * second_num.to_f
+end
+
+def divide(first_num, second_num)
+  first_num.to_f / second_num.to_f
+end
+
+def mod(first_num, second_num)
+  first_num.to_i % second_num.to_i
+end
+
 puts "Simple Calculator"
 25.times { print "-" }
 puts "\nEnter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
-puts "The sum is #{num_1.to_f + num_2.to_f}"
-puts "The difference is #{num_1.to_f - num_2.to_f}"
-puts "The product is #{num_1.to_f * num_2.to_f}"
-puts "The quotient is #{num_1.to_f / num_2.to_f}"
-puts "The remainder is #{num_1.to_i % num_2.to_i}"
+puts "The sum is #{add(num_1, num_2)}"
+puts "The difference is #{subtract(num_1, num_2)}"
+puts "The product is #{multiply(num_1, num_2)}"
+puts "The quotient is #{divide(num_1, num_2)}"
+puts "The remainder is #{mod(num_1, num_2)}"
