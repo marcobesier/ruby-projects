e489fc5db0fc859c86312d3b48626ee0d612e655
Marco Besier <marcobesier@icloud.com>
2020-05-28 07:43:49 +0200

    Add menu for operation selection

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -19,13 +19,32 @@ def mod(first_num, second_num)
 end
 
 puts "Simple Calculator"
+
 25.times { print "-" }
+
 puts "\nEnter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
-puts "The sum is #{add(num_1, num_2)}"
-puts "The difference is #{subtract(num_1, num_2)}"
-puts "The product is #{multiply(num_1, num_2)}"
-puts "The quotient is #{divide(num_1, num_2)}"
-puts "The remainder is #{mod(num_1, num_2)}"
+puts "What do you want to do?"
+puts "Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, and 5 for computing the remainder."
+user_choice = gets.chomp
+
+if user_choice == "1"
+  puts "You have chosen to add the two numbers."
+  puts "The sum is #{add(num_1, num_2)}."
+elsif user_choice == "2"
+  puts "You have chosen to subtract the second number from the first number."
+  puts "The difference is #{subtract(num_1, num_2)}."
+elsif user_choice == "3"
+  puts "You have chosen to multiply both numbers."
+  puts "The product is #{multiply(num_1, num_2)}."
+elsif user_choice == "4"
+  puts "You have chosen to divide the first number by the second number."
+  puts "The quotient is #{divide(num_1, num_2)}."
+elsif user_choice == "5"
+  puts "You have chosen to compute #{num_1} mod #{num_2}."
+  puts "The remainder is #{mod(num_1, num_2)}."
+else
+  puts "Invalid choice."
+end
