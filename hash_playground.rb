3c7076402b793bc592d18fc6e409aa0c3bec5154
Marco Besier <marcobesier@icloud.com>
2020-06-05 07:27:05 +0200

    Add symbol hash

--- hash_playground.rb
+++ hash_playground.rb
@@ -1,2 +1,11 @@
 my_first_hash = {"name" => "Marco", "favcolor" => "blue"}
 puts my_first_hash["name"]
+
+# Create sample hash
+sample_hash = {"a" => 1, "b" => 2, "c" => 3}
+puts sample_hash
+
+# Create hash with symbols as keys, rather than with strings as keys.
+# This is mainly used when the keys are not expected to change.
+another_hash = {a: 1, b: 2, c: 3}
+puts another_hash
