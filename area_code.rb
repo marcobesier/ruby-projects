837b0bcd47f841f41066bff26059e8dd7b72f8a8
Marco Besier <marcobesier@icloud.com>
2020-07-16 08:20:39 +0200

    Add dial book

--- area_code.rb
+++ area_code.rb
@@ -1,3 +1,16 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+
 # Execution flow
 loop do
   puts "Do you want to look up an area code based on a city name?(y/n)"
