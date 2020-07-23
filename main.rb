64617674a289fefa83027f4a6012e0fdaae59c3f
Marco Besier <marcobesier@icloud.com>
2020-07-23 08:36:02 +0200

    Create initial version of hashing module

--- main.rb
+++ main.rb
@@ -1,11 +1,12 @@
 require "bcrypt"
 
 my_password = BCrypt::Password.create("my password")
+my_password_1 = BCrypt::Password.create("my password")
+my_password_2 = BCrypt::Password.create("my password")
 
 puts my_password
-puts my_password.version
-puts my_password.cost
-puts my_password == "my password"
-puts my_password == "not my password"
+puts my_password_1
+puts my_password_2
 
-#my_password = BCrypt::Password.new
+# my_password = BCrypt::Password.new("$2a$12$DYemmJcni6imhgyaCQQ/Q.sdJg.4bq7T7GVLtk4gqQw1W9kQxsMoK")
+# puts my_password == "my password"
